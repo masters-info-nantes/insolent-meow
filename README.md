0316a987bc77954780951f976b067c8eff8df840
kuroidoruido <anthony.pena@outlook.fr>
2015-11-16 17:42:23 +0100

    project structure

---
+++ README.md
@@ -0,0 +1,15 @@
+# insolent-meow
+
+# Compile
+
+```
+mvn -pl m2 clean install
+mvn -pl m1 clean install
+mvn -pl m0 clean install
+```
+
+# Run
+
+```
+mvn -pl m0 exec:java
+```
---
+++ m0/src/main/java/org/alma/csa/insolentmeow/App.java
@@ -0,0 +1,13 @@
+package org.alma.csa.insolentmeow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! M0" );
+    }
+}
