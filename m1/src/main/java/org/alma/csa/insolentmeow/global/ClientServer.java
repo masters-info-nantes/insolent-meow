c71e08c2ccc42c200474c72c258a5d2d42db4d1c
Woohyunnie <marie.lenogue@gmail.com>
2015-12-06 22:47:01 +0100

    Up composition configuration

--- m1/src/main/java/org/alma/csa/insolentmeow/global/ClientServer.java
+++ m1/src/main/java/org/alma/csa/insolentmeow/global/ClientServer.java
@@ -2,13 +2,24 @@ package org.alma.csa.insolentmeow.global;
 
 import org.alma.csa.insolentmeow.IContext;
 import org.alma.csa.insolentmeow.component.Configuration;
+import org.alma.csa.insolentmeow.global.client.Client;
 import org.alma.csa.insolentmeow.global.client.port.ClientLogOutput;
+import org.alma.csa.insolentmeow.global.rpc.RPC;
+import org.alma.csa.insolentmeow.global.server.Serveur;
 
 public class ClientServer extends Configuration {
 
+    Client client;
+    RPC rpc;
+    Serveur serveur;
+    ClientLogOutput clientLogOutput;
+
     public ClientServer(IContext context){
         super(context);
-        ClientLogOutput clientLogOutput = new ClientLogOutput();
+        client = new Client(this);
+        rpc = new RPC(this);
+        serveur = new Serveur(this);
+        clientLogOutput = new ClientLogOutput();
         this.getContext().declare(clientLogOutput,"ClientLogOutput");
     }
 }
--- m1/src/main/java/org/alma/csa/insolentmeow/global/server/Serveur.java
+++ m1/src/main/java/org/alma/csa/insolentmeow/global/server/Serveur.java
@@ -2,13 +2,20 @@ package org.alma.csa.insolentmeow.global.server;
 
 import org.alma.csa.insolentmeow.IContext;
 import org.alma.csa.insolentmeow.component.Configuration;
+import org.alma.csa.insolentmeow.global.server.connexionManager.ConnexionManager;
+import org.alma.csa.insolentmeow.global.server.database.Database;
 import org.alma.csa.insolentmeow.global.server.port.ExternalReceivedRequestPort;
 import org.alma.csa.insolentmeow.global.server.port.ExternalReceivedRequestResponsePort;
+import org.alma.csa.insolentmeow.global.server.securityManager.SecurityManager;
+import org.alma.csa.insolentmeow.global.server.securityQuery.SecurityQuery;
 import org.alma.csa.insolentmeow.global.server.service.ExternalReceivedRequestResponseService;
 import org.alma.csa.insolentmeow.global.server.service.ExternalReceivedRequestService;
 
 public class Serveur extends Configuration {
 
+    ConnexionManager connexionManager;
+    SecurityManager securityManager;
+    Database database;
     ExternalReceivedRequestPort externalReceivedRequestPort;
     ExternalReceivedRequestResponsePort externalReceivedRequestResponsePort;
     ExternalReceivedRequestService externalReceivedRequestService;
@@ -16,6 +23,9 @@ public class Serveur extends Configuration {
 
     public Serveur(IContext context){
         super(context);
+        connexionManager = new ConnexionManager(this);
+        securityManager = new SecurityManager(this);
+        database = new Database(this);
         externalReceivedRequestPort = new ExternalReceivedRequestPort();
         this.getContext().declare(externalReceivedRequestPort,"ExternalReceivedRequestPort");
         externalReceivedRequestResponsePort = new ExternalReceivedRequestResponsePort();
