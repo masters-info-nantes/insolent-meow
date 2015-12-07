9567e24f76c5b44052b169dc1665b58d1365a8d2
kuroidoruido <anthony.pena@outlook.fr>
2015-12-07 10:32:23 +0100

    add glue adder in SimpleConnector + start m0

--- m0/src/main/java/org/alma/csa/insolentmeow/App.java
+++ m0/src/main/java/org/alma/csa/insolentmeow/App.java
@@ -1,13 +1,25 @@
 package org.alma.csa.insolentmeow;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! M0" );
-    }
+// m2
+import org.alma.csa.insolentmeow.IContext;
+import org.alma.csa.insolentmeow.interfaces.connexions.port.*;
+import org.alma.csa.insolentmeow.interfaces.connexions.role.*;
+// m1
+import org.alma.csa.insolentmeow.global.ClientServer;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! M0");
+		ClientServer cs = new ClientServer(new IContext() {
+			public void declare(RequiredPort port, String serviceName){}
+			public void undeclare(RequiredPort port, String serviceName){}
+			public void declare(ProvidedPort port, String serviceName){}
+			public void undeclare(ProvidedPort port, String serviceName){}
+			public void attach(FromRole role, String serviceName){}
+			public void unattach(FromRole role, String serviceName){}
+			public void attach(ToRole role, String serviceName){}
+			public void unattach(ToRole role, String serviceName){}
+		});
+		cs.run();
+	}
 }
--- m2/src/main/java/org/alma/csa/insolentmeow/connector/SimpleConnector.java
+++ m2/src/main/java/org/alma/csa/insolentmeow/connector/SimpleConnector.java
@@ -3,7 +3,18 @@ package org.alma.csa.insolentmeow.connector;
 import org.alma.csa.insolentmeow.IContext;
 
 public class SimpleConnector extends AbstractConnector {
-	public SimpleConnector(IContext context) {
+	private Glue glue;
+	
+	public SimpleConnector(IContext context,Glue glue) {
 		super(context);
+		this.glue = glue;
+	}
+	
+	public Glue getGlue() {
+		return this.glue;
+	}
+	
+	public void setGlue(Glue glue) {
+		this.glue = glue;
 	}
 }
